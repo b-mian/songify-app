ffb883fabd1e50fce82d9f701fbda45f0f62edc3
b-mian <42081057+b-mian@users.noreply.github.com>
2022-01-23 16:50:03 -0500

    ...

--- frontend/src/components/CreatePlaylist.js
+++ frontend/src/components/CreatePlaylist.js
@@ -2,7 +2,6 @@ import React from 'react';
 import useAuth from './useAuth';
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import SpotifyWebApi from 'spotify-web-api-node';
 
 
 
--- frontend/src/components/Forms.js
+++ frontend/src/components/Forms.js
@@ -25,11 +25,12 @@ const Forms = ({code}) => {
 
     const [showPlaylist, setShowPlaylist] = useState(false);
     // perform a check on whether the access token is null; if not, set it
+    spotifyAPI.setAccessToken(accessToken);
     useEffect(() => {
         if (!accessToken) {
             return;
         }
-        spotifyAPI.setAccessToken(accessToken);
+        
     }, [])
 
     // call to api based on search query; return album metadata into an array
