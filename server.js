00e7f6450eb024fe954e08637fbe04a00b0eb624
b-mian <42081057+b-mian@users.noreply.github.com>
2022-01-23 14:25:54 -0500

    ...

--- server.js
+++ server.js
@@ -67,11 +67,13 @@ app.post('/login', (req, res) => {
   })
 })
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-}
+
 
 // port on which app is running locally
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
