ec7f111bf0d9c6f364364c9dc59fc9f925f5df83
b-mian <42081057+b-mian@users.noreply.github.com>
2022-03-27 15:29:37 -0400

    Changed Domain Name and API Redirect

--- frontend/src/components/useAuth.js
+++ frontend/src/components/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
     const [expiresIn, setExpiresIn] = useState();
     useEffect(() => {
         axios
-        .post('http://www.playful-music.com/login', {
+        .post('http://www.playfulmusic.com/login', {
             code
         })
         .then(res => {
@@ -28,7 +28,7 @@ export default function useAuth(code) {
             return;
         }
         axios
-        .post('http://www.playful-music.com/refresh', {
+        .post('http://www.playfulmusic.com/refresh', {
             refreshToken,
         })
         .then(res => {
