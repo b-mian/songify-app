8008ed3b020ea8d3633457547d6832a0a59902b5
b-mian <42081057+b-mian@users.noreply.github.com>
2022-03-27 15:25:24 -0400

    Changed App Domain Name and Redirect URI's

--- frontend/src/components/useAuth.js
+++ frontend/src/components/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
     const [expiresIn, setExpiresIn] = useState();
     useEffect(() => {
         axios
-        .post('http://songify-music.com/login', {
+        .post('http://www.playful-music.com/login', {
             code
         })
         .then(res => {
@@ -28,7 +28,7 @@ export default function useAuth(code) {
             return;
         }
         axios
-        .post('http://songify-music.com/refresh', {
+        .post('http://www.playful-music.com/refresh', {
             refreshToken,
         })
         .then(res => {
